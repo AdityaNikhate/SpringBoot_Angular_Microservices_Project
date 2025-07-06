940c6d8ebddc64ce887eaf8ae81f10fe7e0a3297
AdityaNikhate <nikhateaditya123@gmail.com>
2025-07-06 15:02:27 +0530

    Added the api gateway

--- ProductAndInventoryManagementSystem/order.service/src/main/java/com/adi/microservice/order/service/service/OrderService.java
+++ ProductAndInventoryManagementSystem/order.service/src/main/java/com/adi/microservice/order/service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.adi.microservice.order.service.service;
 
+import com.adi.microservice.order.service.client.InventoryClient;
 import com.adi.microservice.order.service.dto.OrderRequest;
 import com.adi.microservice.order.service.dto.OrderResponse;
 import com.adi.microservice.order.service.model.Order;
@@ -15,15 +16,22 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public OrderResponse placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
-        log.info("{} : Order Created {}",this.getClass().getName(), order.getSkuCode());
-        return new OrderResponse(order.getId(), order.getOrderNumber(), order.getSkuCode(), order.getPrice(), order.getQuantity());
+        boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if(isInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+            log.info("{} : Order Created {}", this.getClass().getName(), order.getSkuCode());
+            return new OrderResponse(order.getId(), order.getOrderNumber(), order.getSkuCode(), order.getPrice(), order.getQuantity());
+        }else{
+            log.error("The order is not in stock");
+            throw new RuntimeException();
+        }
     }
 }
