b1a0e4b5f1f38e5a8ace17ceb2aa937b239d6f37
AdityaNikhate <nikhateaditya123@gmail.com>
2025-07-15 23:55:44 +0530

    updated the swagger and its working also aexcluded the swagger path from api-gateway auth

--- ProductAndInventoryManagementSystem/api.gateway/src/main/java/com/adi/microservice/api/gateway/config/SecurityConfig.java
+++ ProductAndInventoryManagementSystem/api.gateway/src/main/java/com/adi/microservice/api/gateway/config/SecurityConfig.java
@@ -10,7 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest()
                 .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
